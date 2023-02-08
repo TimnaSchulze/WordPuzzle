449d191ff8c48e49be4e6c23a2aff1a2d07c8443
Timna Schulze <Timna.lansel@gmail.com>
2023-02-07 21:02:27 -0800

    add another type of loop to try to replace vowles with __

--- js/script.js
+++ js/script.js
@@ -2,11 +2,20 @@ window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
   event.preventDefault();
-
-
   }
-}
-const pElement = document.createElement("p");
-pElement.append("A puzzle without vowles");
-const firstDiv = document.querySelector("div");
-firstDiv.append(pElement);
+
+
+const vowles = ["a", "e", "i","o", "u"];
+const string = "Figure out the phrase by entering the correct vowles";
+const stringArray = string.split("");
+let xArray = [];
+stringArray.forEach(function(letter) {
+  if (vowles.includes(letter)) {
+    xArray.push("__");
+  } else {
+    xArray.push(letter);
+  }
+});
+const finalString = xArray.join("");
+
+};
